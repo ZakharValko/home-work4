3f78706bed9630a61ea7559bd5c9c986372078db
Zakhar <zahar.valko@gmail.com>
2021-03-10 12:35:05 +0200

    First push

---
+++ src/com/alevel/homeWork4/Task1.java
@@ -0,0 +1,31 @@
+package com.alevel.homeWork4;
+
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // Создаем и наполняем массив случайными числами от 0 до 50
+        int [] array = new int[400];
+        for(int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 50);
+        }
+
+        // Находим сумму значений массива
+        int sum = 0;
+        for(int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+
+        //Находим среднее арифметическое массива
+        int avarageArithmetic = sum/array.length;
+        System.out.println(avarageArithmetic);
+
+        // Находим среднее геометрическое
+        long multiplySum = 1;
+        for(int i = 0; i < array.length; i++) {
+            multiplySum = multiplySum * array[i];
+        }
+        int avarageGeometric = (int) Math.sqrt(multiplySum);
+        System.out.println(avarageGeometric);
+    }
+}
---
+++ src/com/alevel/homeWork4/Task2.java
@@ -0,0 +1,7 @@
+package com.alevel.homeWork4;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+    }
+}
